c3cb3a723260e597e0cc3e4cd0c30c906b6c524c
vladislav-karamfilov <vladislav.karamfilov@gmail.com>
2024-01-09 22:24:29 +0200

    solve puzzle 2 day 2

--- day_2_rock_paper_scissors/src/main.rs
+++ day_2_rock_paper_scissors/src/main.rs
@@ -1,7 +1,59 @@
 fn main() {
-    solve_puzzle1();
+    // solve_puzzle1();
+    solve_puzzle2();
 }
 
+#[allow(dead_code)]
+fn solve_puzzle2() {
+    let mut total_score = 0;
+
+    loop {
+        let mut round = String::new();
+
+        std::io::stdin()
+            .read_line(&mut round)
+            .expect("Failed to read line");
+
+        let round = round.trim();
+        if round.is_empty() {
+            break;
+        }
+
+        let mut hand_shapes_splitter = round.split(' ');
+        let opponent_hand_shape = match hand_shapes_splitter.next().unwrap() {
+            "A" => HandShape::Rock,
+            "B" => HandShape::Paper,
+            _ => HandShape::Scissors,
+        };
+
+        let target_round_outcome = match hand_shapes_splitter.next().unwrap() {
+            "X" => RoundOutcome::Loss,
+            "Y" => RoundOutcome::Draw,
+            _ => RoundOutcome::Win,
+        };
+
+        let my_hand_shape = determine_my_hand_shape(opponent_hand_shape, target_round_outcome);
+
+        let hand_shape_score: i32 = match my_hand_shape {
+            HandShape::Rock => 1,
+            HandShape::Paper => 2,
+            _ => 3,
+        };
+
+        let round_outcome_score: i32 = match target_round_outcome {
+            RoundOutcome::Win => 6,
+            RoundOutcome::Draw => 3,
+            _ => 0,
+        };
+
+        total_score += hand_shape_score;
+        total_score += round_outcome_score;
+    }
+
+    println!("{total_score}");
+}
+
+#[allow(dead_code)]
 fn solve_puzzle1() {
     let mut total_score = 0;
 
@@ -51,6 +103,29 @@ fn solve_puzzle1() {
     println!("{total_score}");
 }
 
+fn determine_my_hand_shape(
+    opponent_hand_shape: HandShape,
+    target_round_outcome: RoundOutcome,
+) -> HandShape {
+    if target_round_outcome == RoundOutcome::Win {
+        return match opponent_hand_shape {
+            HandShape::Rock => HandShape::Paper,
+            HandShape::Paper => HandShape::Scissors,
+            HandShape::Scissors => HandShape::Rock,
+        };
+    }
+
+    if target_round_outcome == RoundOutcome::Loss {
+        return match opponent_hand_shape {
+            HandShape::Rock => HandShape::Scissors,
+            HandShape::Paper => HandShape::Rock,
+            HandShape::Scissors => HandShape::Paper,
+        };
+    }
+
+    opponent_hand_shape
+}
+
 fn determine_round_outcome(
     opponent_hand_shape: HandShape,
     my_hand_shape: HandShape,
@@ -69,6 +144,7 @@ fn determine_round_outcome(
     RoundOutcome::Draw
 }
 
+#[derive(Clone, Copy, PartialEq)]
 enum RoundOutcome {
     Win,
     Draw,
