da82cc7f1f31be09e25f0ab4f3461a66384ccf6e
vladislav-karamfilov <vladislav.karamfilov@gmail.com>
2024-01-29 22:10:20 +0200

    solve puzzle 2 day 10

--- day_10_cathode_ray_tube/src/main.rs
+++ day_10_cathode_ray_tube/src/main.rs
@@ -1,11 +1,63 @@
 use std::io::{Error, ErrorKind};
 
 fn main() {
-    solve_puzzle1();
+    // solve_puzzle1();
+    solve_puzzle2();
 }
 
+#[allow(dead_code)]
+fn solve_puzzle2() {
+    let mut result_image = String::with_capacity(240);
+
+    let mut x_register_value = 1;
+    let mut drawn_col_position = 0;
+
+    loop {
+        let mut line = String::new();
+
+        std::io::stdin()
+            .read_line(&mut line)
+            .expect("Failed to read line");
+
+        let line = line.trim();
+        if line.is_empty() {
+            break;
+        }
+
+        let (instruction, raw_value) = line.split_at(4);
+
+        let instruction_result =
+            handle_instruction(instruction, raw_value, x_register_value, &mut || {
+                if drawn_col_position == x_register_value - 1
+                    || drawn_col_position == x_register_value
+                    || drawn_col_position == x_register_value + 1
+                {
+                    result_image.push('#');
+                } else {
+                    result_image.push('.');
+                }
+
+                if drawn_col_position > 0 && drawn_col_position % 39 == 0 {
+                    drawn_col_position = 0;
+
+                    result_image.push('\n');
+                } else {
+                    drawn_col_position += 1;
+                }
+            });
+
+        match instruction_result {
+            Ok(v) => x_register_value = v,
+            Err(err) => panic!("{err}"),
+        }
+    }
+
+    println!("{result_image}");
+}
+
+#[allow(dead_code)]
 fn solve_puzzle1() {
-    let mut x_register_values = Vec::with_capacity(220);
+    let mut signal_strengths = Vec::with_capacity(220);
 
     let mut x_register_value = 1;
 
@@ -23,41 +75,44 @@ fn solve_puzzle1() {
 
         let (instruction, raw_value) = line.split_at(4);
 
-        match handle_instruction(
-            instruction,
-            raw_value,
-            x_register_value,
-            &mut x_register_values,
-        ) {
+        let instruction_result =
+            handle_instruction(instruction, raw_value, x_register_value, &mut || {
+                signal_strengths.push(x_register_value * (signal_strengths.len() as i32 + 1))
+            });
+
+        match instruction_result {
             Ok(v) => x_register_value = v,
             Err(err) => panic!("{err}"),
         }
     }
 
-    let sum = (x_register_values[19] * 20)
-        + (x_register_values[59] * 60)
-        + (x_register_values[99] * 100)
-        + (x_register_values[139] * 140)
-        + (x_register_values[179] * 180)
-        + (x_register_values[219] * 220);
+    let sum = signal_strengths[19]
+        + signal_strengths[59]
+        + signal_strengths[99]
+        + signal_strengths[139]
+        + signal_strengths[179]
+        + signal_strengths[219];
 
     println!("{sum}");
 }
 
-fn handle_instruction(
+fn handle_instruction<F>(
     instruction: &str,
     raw_value: &str,
     x_register_value: i32,
-    x_register_values: &mut Vec<i32>,
-) -> Result<i32, Error> {
+    cycle_action: &mut F,
+) -> Result<i32, Error>
+where
+    F: FnMut(),
+{
     let mut new_x_register_value = x_register_value;
 
     match instruction {
-        "noop" => x_register_values.push(x_register_value),
+        "noop" => cycle_action(),
 
         "addx" => {
-            x_register_values.push(x_register_value);
-            x_register_values.push(x_register_value);
+            cycle_action();
+            cycle_action();
 
             let value = match raw_value.trim().parse::<i32>() {
                 Ok(v) => v,
