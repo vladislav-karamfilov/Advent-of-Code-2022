3ed921eff0da882e760c9dc7827255b0ca72e2c8
vladislav-karamfilov <vladislav.karamfilov@gmail.com>
2024-03-29 22:54:47 +0200

    wip on faster sln of puzzle 1 day 15

--- day_15_beacon_exclusion_zone/src/main.rs
+++ day_15_beacon_exclusion_zone/src/main.rs
@@ -1,87 +1,148 @@
-use std::collections::HashSet;
-
 fn main() {
-    // solve_puzzle1(10);
-    solve_puzzle1(2000000);
+    solve_puzzle1(10);
+    // solve_puzzle1(2000000);
 }
 
 fn solve_puzzle1(target_y: i32) {
     let (sensor_coords, beacon_coords) = read_sensor_and_closest_beacon_coordinates();
 
-    let mut covered_coordinates: HashSet<(i32, i32)> = HashSet::new();
+    let mut min_x = i32::MAX;
+    let mut max_x = i32::MIN;
+    let mut all_covered_areas = vec![];
+
     for (i, sensor_coord) in sensor_coords.iter().enumerate() {
         let beacon_coord = beacon_coords[i];
 
         let manhattan_distance = calculate_manhattan_distance(*sensor_coord, beacon_coord);
 
-        // The whole "manhattan area" won't cross the target Y
-        let min_y = sensor_coord.1.min(beacon_coord.1) - manhattan_distance as i32;
-        let max_y = sensor_coord.1.max(beacon_coord.1) + manhattan_distance as i32;
-        if min_y > target_y || max_y < target_y {
+        let covered_area_edges =
+            calculate_covered_area_edges(sensor_coord.0, sensor_coord.1, manhattan_distance);
+
+        let min_covered_x = covered_area_edges
+            .iter()
+            .map(|coord| coord.0)
+            .min()
+            .unwrap();
+
+        if min_x > min_covered_x {
+            min_x = min_covered_x;
+        }
+
+        let max_covered_x = covered_area_edges
+            .iter()
+            .map(|coord| coord.0)
+            .max()
+            .unwrap();
+
+        if max_x < max_covered_x {
+            max_x = max_covered_x;
+        }
+
+        all_covered_areas.push(covered_area_edges);
+
+        print_area(&sensor_coords, &beacon_coords, &all_covered_areas);
+        println!();
+        println!();
+    }
+
+    // print_area(&sensor_coords, &beacon_coords, &all_covered_areas);
+
+    let mut covered_x = 0;
+
+    for x in min_x..=max_x {
+        let coord = (x, target_y);
+        if sensor_coords.contains(&coord) || beacon_coords.contains(&coord) {
             continue;
         }
 
-        let manhattan_area_coordinates =
-            get_coordinates_in_manhattan_area(sensor_coord.0, sensor_coord.1, manhattan_distance);
-
-        covered_coordinates.extend(&manhattan_area_coordinates);
+        if is_coordinate_inside_area(&coord, &all_covered_areas) {
+            covered_x += 1;
+        }
     }
 
-    let covered_x = covered_coordinates
-        .iter()
-        .filter(|coord| {
-            coord.1 == target_y && !sensor_coords.contains(coord) && !beacon_coords.contains(coord)
-        })
-        .count();
-
     println!("{covered_x}");
+}
+
+fn print_area(
+    sensor_coords: &[(i32, i32)],
+    beacon_coords: &[(i32, i32)],
+    all_covered_areas: &[Vec<(i32, i32)>],
+) {
+    for y in -10..32 {
+        for x in -20..32 {
+            let coord = (x, y);
+            if sensor_coords.contains(&coord) {
+                print!("S");
+            } else if beacon_coords.contains(&coord) {
+                print!("B");
+            } else if all_covered_areas
+                .iter()
+                .any(|coords| coords.contains(&coord))
+            {
+                print!("V");
+            } else if is_coordinate_inside_area(&coord, all_covered_areas) {
+                print!("#");
+            } else {
+                print!(".");
+            }
+        }
+
+        println!();
+    }
+}
+
+fn is_coordinate_inside_area(coord: &(i32, i32), all_covered_areas: &[Vec<(i32, i32)>]) -> bool {
+    for covered_area_edges in all_covered_areas {
+        if covered_area_edges.contains(coord) {
+            return true;
+        }
+
+        // TODO: Check if on a line?
+        let mut inside = false;
+        let mut i = 0;
+        let mut j = covered_area_edges.len() - 1;
+
+        while i < covered_area_edges.len() {
+            if (covered_area_edges[i].1 > coord.1) != (covered_area_edges[j].1 > coord.1)
+                && (coord.0 as i64)
+                    < (covered_area_edges[j].0 - covered_area_edges[i].0) as i64
+                        * (coord.1 - covered_area_edges[i].1) as i64
+                        / (covered_area_edges[j].1 - covered_area_edges[i].1) as i64
+                        + covered_area_edges[i].0 as i64
+            {
+                inside = !inside;
+            }
+
+            j = i;
+            i += 1;
+        }
+
+        if inside {
+            return true;
+        }
+    }
+
+    false
 }
 
 fn calculate_manhattan_distance(start: (i32, i32), end: (i32, i32)) -> u32 {
     start.0.abs_diff(end.0) + start.1.abs_diff(end.1)
 }
 
-fn get_coordinates_in_manhattan_area(
+fn calculate_covered_area_edges(
     start_x: i32,
     start_y: i32,
     manhattan_distance: u32,
 ) -> Vec<(i32, i32)> {
-    let mut result = Vec::with_capacity(4 * manhattan_distance as usize);
-
-    let manhattan_distance = manhattan_distance as i32;
+    let mut result = Vec::with_capacity(5);
 
     // Approach: https://stackoverflow.com/questions/75128474/how-to-generate-all-of-the-coordinates-that-are-within-a-manhattan-distance-r-of#answer-75129338
-    for offset in 0..manhattan_distance {
-        let inverse_offset = manhattan_distance - offset;
-        result.push((start_x + offset, start_y + inverse_offset));
-        result.push((start_x + inverse_offset, start_y - offset));
-        result.push((start_x - offset, start_y - inverse_offset));
-        result.push((start_x - inverse_offset, start_y + offset));
-    }
-
-    let min_y = result.iter().map(|coord| coord.1).min().unwrap();
-    let max_y = result.iter().map(|coord| coord.1).max().unwrap();
-
-    // Fill in the star-like shape
-    for y in min_y + 1..max_y {
-        let min_x = result
-            .iter()
-            .filter(|coord| coord.1 == y)
-            .map(|coord| coord.0)
-            .min()
-            .unwrap();
-
-        let max_x = result
-            .iter()
-            .filter(|coord| coord.1 == y)
-            .map(|coord| coord.0)
-            .max()
-            .unwrap();
-
-        for x in min_x + 1..max_x {
-            result.push((x, y));
-        }
-    }
+    let max_offset = manhattan_distance as i32;
+    result.push((start_x + max_offset, start_y));
+    result.push((start_x, start_y - max_offset));
+    result.push((start_x - max_offset, start_y));
+    result.push((start_x, start_y + max_offset));
+    result.push((start_x + max_offset, start_y));
 
     result
 }
